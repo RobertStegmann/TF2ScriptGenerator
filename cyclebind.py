571bb40b707637a53d9a43508a0d38d2b5a0cef9
Robert Stegmann <rstegman@uoguelph.ca>
2023-12-10 12:35:35 -0500

    Bug fixes

--- cyclebind.py
+++ cyclebind.py
@@ -50,29 +50,38 @@ def main():
 def generateCycleBind(input,key,alias,cycle,output,wait):
     inputFile = open(input, "r")
     outputFile = open(output, "w")
+    outputFile.write("// Place this in your autoexec.cfg file or any .cfg file you'd like\n")
+    outputFile.write("// Every time that .cfg is ran (For example, every time the game is launched for autoexec.cfg), the cycle starts from the beginning\n")
+    outputFile.write("// The binds will go in order, there is no randomization\n")
+    outputFile.write(f"// If you would like to jump to somewhere in the list, type 'alias {alias} \"{cycle}[whatever number here]\"' in the developer console\n")
+    outputFile.write("\n")
+    outputFile.write(f"bind {key} \"{alias}\"\n")
+    outputFile.write("\n")
     i = 0
     outputFile.write(f"alias {alias} \"{cycle}0\"\n")
     for input_line in inputFile:
-        if i > 0:
-            outputFile.write(f"alias {alias} {cycle}{i}\"\n")
         line = input_line.strip().replace("\"","\'")
         if len(line) > 0:
-            if len(line) <= 127:
-                outputFile.write(f"alias {cycle}{i} \"say {line};")   
-            else:
-                lines = splitLines(line)
-                length = len(lines)
-                if length > 0:
-                    for j in range(1,length):
-                        outputFile.write(f"alias {alias}{i}{j} \"wait {wait}; say {lines[j]}; {alias}{i}{j+1};\"\n") 
-                    outputFile.write(f"alias {cycle}{i} \"say {lines[0]}; {alias}{i}1;") 
+            if i > 0:
+                outputFile.write(f"alias {alias} {cycle}{i}\"\n") 
+            if len(line) > 0:
+                if len(line) <= 127:
+                    outputFile.write(f"alias {cycle}{i} \"say {line};")   
+                else:
+                    lines = splitLines(line)
+                    length = len(lines)
+                    if length > 0:
+                        for j in range(1,length):
+                            if j > 1:
+                                outputFile.write(f";{alias}{i}{j+1};\"\n")
+                            outputFile.write(f"alias {alias}{i}{j} \"wait {wait}; say {lines[j]}")
+                        outputFile.write(f"\"\nalias {cycle}{i} \"say {lines[0]}; {alias}{i}1;") 
                 
         i += 1
     
     inputFile.close()
         
-    outputFile.write(f"alias {alias} \"{cycle}0\"\n")
-    outputFile.write(f"bind {key} \"{alias}\"\n")
+    outputFile.write(f"alias {alias} {cycle}0\"\n")
     
     outputFile.close()
     
@@ -86,7 +95,7 @@ def splitLines(line):
             for i in range(0,len(split_lines)):
                 if i < len(split_lines) - 1:
                     split_lines[i] = split_lines[i] + seperator.strip()
-                if len(line) > 127:
+                if len(split_lines[i]) > 127:
                     correct_split = False
             
             if correct_split:
@@ -114,8 +123,12 @@ def splitLines(line):
             splits.append(correct_line)
             correct_line = word
         else:
-            correct_line = f"{correct_line} {word}"
-    
+            if len(correct_line) > 0:
+                correct_line = f"{correct_line} {word}"
+            else:
+                correct_line = word
+    if len(correct_line) > 0:
+        splits.append(correct_line)
     return splits                
 
 if __name__ == "__main__":
